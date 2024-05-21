e56ef99a75a003713640297f631034c96622f2ae
Kushika <kushikaagarwalg1080@gmail.com>
2024-05-21 16:50:41 +0530

    may be

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,31 +5,7 @@ import Link from 'next/link'
 function Navbar() {
 
   return (
-//     <div className='bg-yellow-100'>
-//       <div className="navbar relative justify-between shadow-xl bg-yellow-200 rounded-lg  flex-auto md:flex">
-//    <Image src="/logo.png" width={100} height={100} alt='logo' className='ml-8  -m-3'></Image>
-//    <div classNamestart='md:flex font-semibold  px-8 '>
-    
-//     {/* <Link className=' p-3 hover:font-bold text-lg cursor-pointer' href="/">Home</Link> */}
-//     <div className=" dropdown dropdown-hover">
-//   <Link  href="/" className="font-bold  mx-1  md:mx-10  rounded-lg   p-3 hover:font-bold text-lg cursor-pointer">Home</Link>
-//   <ul  className="dropdown-content z-10 absolute menu p-2  w-20">
-//     <Link href="/#journey" className=' hover:bg-white bg-orange-400 p-3 hover:font-bold text-lg  rounded-md cursor-pointer'>Journey</Link>
-//     <Link href="/#services" className=' hover:bg-white bg-orange-400 p-3 hover:font-bold text-lg rounded-md cursor-pointer' >Services</Link>
-//     <Link href="/#review" className=' hover:bg-white bg-orange-400 p-3 hover:font-bold text-lg rounded-md cursor-pointer' >Reviews</Link>
-//     <Link href="/#achievements" className=' hover:bg-white bg-orange-400  p-3 hover:font-bold text-lg rounded-md cursor-pointer' >Achievements</Link>
-//   </ul>
-// </div>
-//     <Link className='mx-1  md:mx-10  rounded-lg p-3 hover:font-bold text-lg cursor-pointer' href="/About">About</Link>
-//     <Link className='mx-1  md:mx-10  rounded-lg p-3 hover:font-bold text-lg cursor-pointer' href="/Contact">Contact</Link>
-//     {/* <Link className='mx-1  md:mx-10  rounded-lg p-3 hover:font-bold text-lg cursor-pointer' href="/Services">Services</Link> */}
-//     {/* <Link className='mx-1 sm:mx-5 md:mx-10  rounded-lg p-3 hover:hover:font-bold text-lg cursor-pointer' href="/#review">Reviews</Link> */}
-//     {/* <Link className='mx-1 md:mx-10  rounded-lg p-3 hover:hover:font-bold text-lg cursor-pointer' href="/#achievements">Achievements</Link> */}
-    
 
-//    </div>
-// </div>
-//     </div>
 <><div className='bg-yellow-100'>
 <div className="navbar shadow-xl bg-yellow-200 md:hidden py-2 ">
   <div className="navbar-start font-semibold ">
@@ -67,7 +43,7 @@ function Navbar() {
     <Image src="/logo.png" width={100} height={100} alt='logo' className='ml-8  -m-3'></Image>
     <div classNamestart='md:flex font-semibold  px-8 '>
     
-     {/* <Link className=' p-3 hover:font-bold text-lg cursor-pointer' href="/">Home</Link> */}
+  
      <div className=" dropdown dropdown-hover">
    <Link  href="/" className="font-bold  mx-1  md:mx-10  rounded-lg   p-3 hover:font-bold hover:bg-orange-400 text-lg cursor-pointer">Home</Link>
    <ul  className="dropdown-content z-10 absolute menu p-2  w-20">
@@ -89,21 +65,6 @@ function Navbar() {
 </div>
 
 
-  {/* <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div> */}
   
 
 </>
